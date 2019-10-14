c565c1c7c104276179fbb8b7e03cae6c72d8f1ce
Sofiane <sofiane.ouafir@live.fr>
2019-10-14 19:49:04 -0400

    update loader component

--- app/javascript/components/Loader.js
+++ app/javascript/components/Loader.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { ClipLoader } from 'react-spinners';
 
 const Loader = () => (
-  <div>
-    loading
+  <div class="fullscreen flex justify-content--between align-items--center">
+    <div className="fullwidth flex justify-content--center">
+      <ClipLoader
+        size={100}
+        loading={true}
+        color={'#506C9D'}
+      />
+    </div>
   </div>
 );
 
