dd643a25765aa8321a13e99c904e4bfff9f23fc6
Sofiane <sofiane.ouafir@live.fr>
2019-11-02 02:10:02 -0400

    Should not be able to get deliverable back when deliverable is printed. api/v1/partners/deliverables/show

--- app/controllers/api/v1/partners/deliverables_controller.rb
+++ app/controllers/api/v1/partners/deliverables_controller.rb
@@ -5,7 +5,11 @@ class Api::V1::Partners::DeliverablesController < ApplicationController
     OpenURI::Buffer.send :remove_const, 'StringMax' if OpenURI::Buffer.const_defined?('StringMax')
     OpenURI::Buffer.const_set 'StringMax', 0
     deliverable = Deliverable.find(params[:id])
-    pdf_content = open(deliverable.file.service_url)
-    send_file(pdf_content, filename: 'something', disposition: 'inline', type: 'application/pdf')
+    if deliverable.order.printer_id.present?
+      render json: { message: 'This action is not allowed' }, status: 403
+    else
+      pdf_content = open(deliverable.file.service_url)
+      send_file(pdf_content, filename: 'something', disposition: 'inline', type: 'application/pdf')
+    end
   end
 end
