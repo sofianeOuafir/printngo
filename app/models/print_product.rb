33e60ba4950edc875a7c6b5ebe3f0e895668f944
Sofiane <sofiane.ouafir@live.fr>
2019-11-22 12:26:23 -0500

    Update print_product.rb

--- app/models/print_product.rb
+++ app/models/print_product.rb
@@ -1,9 +1,9 @@
 class PrintProduct < Product
-  def self.a4_color
+  def self.a4_black_and_white
     PrintProduct.find_by(name: 'A4 Black')
   end
 
-  def self.a4_black_and_white
+  def self.a4_color
     PrintProduct.find_by(name: 'A4 Color')
   end
 
