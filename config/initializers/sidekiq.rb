50059c1c1215683385457e536dc7471242f0aea7
Sofiane <sofiane.ouafir@live.fr>
2019-11-22 23:26:28 -0500

    update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,9 @@
-if Rails.env.development?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379' }
-  end
+require 'sidekiq'
+
+Sidekiq.configure_client do |config|
+config.redis = { :size => 1 }
 end
 
-if Rails.env.production?
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDISCLOUD_URL'] }
-  end
-
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDISCLOUD_URL'] }
-  end
+Sidekiq.configure_server do |config|
+config.redis = { :size => 4 }
 end
