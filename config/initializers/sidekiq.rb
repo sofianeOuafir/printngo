a7391a9aa9b794b4beb5df3c3439ed94d7ae6736
Sofiane <sofiane.ouafir@live.fr>
2019-11-22 23:32:41 -0500

    Revert "update"
    This reverts commit 50059c1c1215683385457e536dc7471242f0aea7.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,15 @@
-require 'sidekiq'
-
-Sidekiq.configure_client do |config|
-config.redis = { :size => 1 }
+if Rails.env.development?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://localhost:6379' }
+  end
 end
 
-Sidekiq.configure_server do |config|
-config.redis = { :size => 4 }
+if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDISCLOUD_URL'] }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDISCLOUD_URL'] }
+  end
 end
