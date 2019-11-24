f4a090fd3b231f9352e0be6ea627abe67e828b03
Sofiane <sofiane.ouafir@live.fr>
2019-11-23 19:17:33 -0500

    protect sidekiq UI with password

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,6 @@
+require 'sidekiq'
+require 'sidekiq/web'
+
 if Rails.env.development?
   Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://localhost:6379' }
@@ -5,6 +8,10 @@ if Rails.env.development?
 end
 
 if Rails.env.production?
+  Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+    [user, password] == [ENV['SIDEKIQ_UI_USER'], ENV['SIDEKIQ_UI_PASSWORD']]
+  end
+
   Sidekiq.configure_client do |config|
     config.redis = { url: ENV['REDISCLOUD_URL'] }
   end
