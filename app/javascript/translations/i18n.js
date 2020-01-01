ab93dda0b4e9ec878b07a9889d9f26b7ea37c676
Sofiane <sofiane.ouafir@live.fr>
2020-01-01 12:30:58 -0500

    detectionOptions update

--- app/javascript/translations/i18n.js
+++ app/javascript/translations/i18n.js
@@ -6,8 +6,30 @@ import translationEN from "./locales/en/translations.json";
 import translationFR from "./locales/fr/translations.json";
 
 const detectionOptions = {
-  order: ["subdomain", "cookie", "navigator"]
-};
+  // order and from where user language should be detected
+  order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+
+  // keys or params to lookup language from
+  lookupQuerystring: 'lng',
+  lookupCookie: 'i18next',
+  lookupLocalStorage: 'i18nextLng',
+  lookupFromPathIndex: 0,
+  lookupFromSubdomainIndex: 0,
+
+  // cache user language on
+  caches: ['localStorage', 'cookie'],
+  excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+
+  // optional expire and domain for set cookie
+  cookieMinutes: 10,
+  cookieDomain: 'myDomain',
+
+  // optional htmlTag with lang attribute, the default is:
+  htmlTag: document.documentElement,
+
+  // only detect languages that are in the whitelist
+  checkWhitelist: true
+}
 
 const resources = {
   en: {
