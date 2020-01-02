1409276a9a9b993db00a4ac172bc340e325cf377
Sofiane <sofiane.ouafir@live.fr>
2020-01-02 13:32:13 -0500

    add TLD detector

--- app/javascript/translations/i18n.js
+++ app/javascript/translations/i18n.js
@@ -1,16 +1,30 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import tldjs from "tldjs";
 
 import translationEN from "./locales/en/translations.json";
 import translationFR from "./locales/fr/translations.json";
+const tldDetector = {
+  name: "tldDetector",
+
+  lookup() {
+    const { tldExists, getPublicSuffix } = tldjs;
+    const tld = tldExists(window.location) ? getPublicSuffix(window.location) : 'en'
+    return tld;
+  }
+};
+
+const languageDetector = new LanguageDetector();
+languageDetector.addDetector(tldDetector);
 
 const detectionOptions = {
   // order and from where user language should be detected
-  order: ['subdomain'],
+  order: ["tldDetector"],
+
   // only detect languages that are in the whitelist
   checkWhitelist: true
-}
+};
 
 const resources = {
   en: {
@@ -21,7 +35,7 @@ const resources = {
   }
 };
 i18n
-  .use(LanguageDetector)
+  .use(languageDetector)
   .use(initReactI18next)
   .init({
     detection: detectionOptions,
